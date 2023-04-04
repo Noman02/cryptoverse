1981a48bdf75b7a7e20f6456c289a45be1c805d0
Noman02 <noman.mahib001@gmail.com>
2023-04-04 17:48:55 +0600

    almost doen

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import {Button,Menu,Typography,Avatar}from "antd"
 import { HomeOutlined,MoneyCollectOutlined,BulbOutlined,FundOutlined,MenuOutlined } from '@ant-design/icons'
 import {Link} from "react-router-dom"
@@ -6,6 +6,27 @@ import cryptocurrency from "../assets/cryptocurrency.png"
 import MenuItem from 'antd/es/menu/MenuItem'
 
 const Navbar = () => {
+    const [activeMenu,setActiveMenu]=useState(true)
+    const [screenSize,setScreenSize]=useState(null)
+
+    useEffect(()=>{
+        const handleResize= ()=> setScreenSize(window.innerWidth)
+        window.addEventListener("resize",handleResize)
+
+        handleResize()
+
+        return ()=> window.removeEventListener("resize",handleResize)
+    },[])
+
+    useEffect(()=>{
+        if(screenSize<768){
+            setActiveMenu(false)
+        }
+        else{
+            setActiveMenu(true)
+        }
+    },[screenSize])
+
   return (
     <div className='nav-container'>
         <div className='logo-container'>
@@ -13,9 +34,13 @@ const Navbar = () => {
             <Typography.Title className='logo' level={2}>
                 <Link to="/">Cryptoverse</Link>
             </Typography.Title>
-            {/* <Button className='menu-control-container'></Button> */}
+            
+            <Button className="menu-control-container" onClick={() => setActiveMenu(!activeMenu)}><MenuOutlined /></Button>
 
         </div>
+        {
+            activeMenu && (
+
         <Menu theme='dark'>
             <Menu.Item icon={<HomeOutlined/>}>
                 <Link to="/">Home</Link>
@@ -29,8 +54,9 @@ const Navbar = () => {
             <Menu.Item icon={<BulbOutlined/>}>
                 <Link to="/news">News</Link>
             </Menu.Item>
-
         </Menu>
+            )
+        }
     </div>
   )
 }
--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -3,6 +3,7 @@ import {createApi,fetchBaseQuery}from "@reduxjs/toolkit/query/react"
 const cryptoApiHeaders= {
     'X-RapidAPI-Host': 'coinranking1.p.rapidapi.com',
     'X-RapidAPI-Key': '6737badcefmshbc9e2dfceaa306ap1edeebjsndaccf273f75d'
+                        
 }
 
 const baseUrl= 'https://coinranking1.p.rapidapi.com'
@@ -18,8 +19,11 @@ export const cryptoApi= createApi({
         }),
         getCryptoDetails: builder.query({
             query:(coinId => createRequest(`/coin/${coinId}`))
-        })
+        }),
+        getCryptoHistory: builder.query({
+            query: ({ coinId, timeperiod }) => createRequest(`coin/${coinId}/history?timeperiod=${timeperiod}`),
+          }),
     })
 })
 
-export const {useGetCryptosQuery,useGetCryptoDetailsQuery}=cryptoApi
+export const {useGetCryptosQuery,useGetCryptoDetailsQuery,useGetCryptoHistoryQuery}=cryptoApi
